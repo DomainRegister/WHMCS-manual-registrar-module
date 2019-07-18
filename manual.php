1dd554ecd536872cf01cede11791af32a3380646
Remitur <franco.tauceri@gmail.com>
2019-07-18 08:40:28 +0200

    Update manual.php

--- manual.php
+++ manual.php
@@ -1,5 +1,29 @@
 <?php
 
+/* * ********************************************************************
+ * 
+ * Copyright (c) DomainRegister, PERSOLVO d.o.o. -  All Rights Reserved 
+ * 
+ * 
+ *
+ *  DomainRegister        https://domainregister.international
+ *  CONTACT                        ->       support@domainregister.it
+ *
+ *  github: https://github.com/DomainRegister
+ *
+ *
+ * This software is furnished under a license and may be used and copied
+ * only  in  accordance  with  the  terms  of such  license and with the
+ * inclusion of the above copyright notice.  This software  or any other
+ * copies thereof may not be provided or otherwise made available to any
+ * other person.  No title to and  ownership of the  software is  hereby
+ * transferred.
+ *
+ *
+ *      software version 1.00
+ *      software release 20190718a
+ * ******************************************************************** */
+
 if (!defined("WHMCS")) {
     die("This file cannot be accessed directly");
 }
